0b9ef564880610d6263a38faeaa5a573d10ffdb3
root <root@web-temp.ngw>
2015-03-15 22:08:47 +0000

    Introduced scroll between posts.

--- wp-content/themes/rubberfish/blog-template.php
+++ wp-content/themes/rubberfish/blog-template.php
@@ -2,45 +2,49 @@
 Template Name: blog-template
 */ ?> 
     <?php get_header(); ?> 
-    <script>
+<script>
 
-    function transition_in (x) {
-        $ (x) .css ('display', 'block');
-        $ (x) .css ('visibility', 'visible');
-        $ (x) .fadeTo (500, 1);
+function transition_in (x) {
+    $ (x) .css ('display', 'block');
+    $ (x) .css ('visibility', 'visible');
+    $ (x) .fadeTo (500, 1);
+}
+
+function transition_out (x) {
+    $ (x) .fadeTo (500, 0);
+    $ (x) .css ('display', 'none');
+}
+
+function transition (target_screen) {
+    if (current_screen != target_screen) {
+
+
+        transition_out (current_screen);
+        transition_in (target_screen);
+        current_screen = target_screen;
+        $(window) .resize();
     }
-
-    function transition_out (x) {
-        $ (x) .fadeTo (500, 0);
-        $ (x) .css ('display', 'none');
-    }
-
-    function transition (target_screen) {
-        if (current_screen != target_screen) {
-            transition_out (current_screen);
-            transition_in (target_screen);
-            current_screen = target_screen;
-        }
-    }
+}
 
 
-    function Clone(x) {
-           for(p in x)
-               this[p] = (typeof(x[p]) == 'object')? new Clone(x[p]) : x[p];
-    }
+function Clone(x) {
+       for(p in x)
+           this[p] = (typeof(x[p]) == 'object')? new Clone(x[p]) : x[p];
+}
 
-    var contents = [] ;
-    var current_screen = '' ;
+var contents = [] ;
+var current_screen = '' ;
 
-    </script>
-    <div>
-        <ul class='blog-menu'>
+</script>
+
+<div>
+    <div class='nav blog-menu margins'>
 <?php if (is_category('frontpage')):
-echo '<li><span>BEinDESIGN</span></li>';
+echo '<div class="nav-elem inline-block"><span>BEinDESIGN</span></div>';
 else:
-    echo '<li><a href="'.get_term_link('frontpage', 'category') .'">BEinDESIGN</a></li>';
+    echo '<div class="nav-elem inline-block"><a href="'.get_term_link('frontpage', 'category') .'">BEinDESIGN</a></div>';
 endif; ?>
-        </ul>
+        </div>
     </div>
     <div  class="content-area blog-wr" >
     <?php while ( have_posts() ) : the_post(); ?>
@@ -50,70 +54,88 @@ endif; ?>
     </div><!-- content-area -->
 <script>
 
-                for (x in contents) {
-                    $ ('.blog-menu') .append (
-                        '<li id="blog-menu-' + contents[x]['slug']
-                        + '">' + contents[x]['title']
-                        + '</li>'
-                    );
+for (x in contents) {
+    $ ('.blog-menu') .append (
+        '<div class="nav-elem inline-block" id="blog-menu-' + contents[x]['slug']
+        + '">' + contents[x]['title']
+        + '</div>'
+    );
 
-                    function closure (target_slug) {
-                        function tempora () {
-                            var target_screen = '#post-'+target_slug;
-                            transition (target_screen);
-                        }
-                        return tempora;
+    function closure (target_slug) {
+        function tempora () {
+            var target_screen = '#post-'+target_slug;
+            transition (target_screen);
+        }
+        return tempora;
+    }
+
+    $ ('#blog-menu-' + contents [x] ['slug']) .click (
+        closure (contents [x] ['slug'])
+    );
+
+
+};
+
+// Initialize first screen.
+if (window.location.hash) { current_screen = window.location.hash ; }
+else { current_screen = '#post-' + contents [0] ['slug']; }
+$(document) .ready ( function () {
+    setTimeout (transition_in (current_screen, current_screen), 1000); //???
+    scroll_listener();
+}
+);
+
+function scroll_listener () {
+    scroll_value = 0;
+    var delta = 3;
+    $(window) .bind ('DOMMouseScroll mousewheel', function (event) {
+        var scroll_unit_firefox = event.originalEvent.wheelDelta / -120;
+        var scroll_unit_chrome = event.originalEvent.detail / 3;
+        
+        if (! isNaN(scroll_unit_firefox) ) { console.log (scroll_unit_firefox) ; scroll_value = scroll_value + parseInt (scroll_unit_firefox) };
+        if (scroll_unit_chrome !== NaN) { scroll_value = scroll_value + parseInt (scroll_unit_chrome) };
+        if (Math.abs(scroll_value) > delta) {
+            for (x in contents) {
+                if ('#post-' + contents [x] ['slug'] === current_screen) {
+                    if (parseInt(x) + Math.sign(scroll_value) in contents) {
+                        console.log ('scrolling to ' + x + ' with scroll ' + (Math.sign(scroll_value)) + '.');
+                        console.log ('target slug ' + ('#post-' + contents [parseInt(x)+Math.sign (scroll_value)] ['slug']) + '.');
+                        transition ('#post-' + contents [parseInt(x)+Math.sign (scroll_value)] ['slug']);
                     }
-
-                    $ ('#blog-menu-' + contents [x] ['slug']) .click (
-                        closure (contents [x] ['slug'])
-                    );
-
-
-                };
-
-                // Initialize first screen.
-                if (window.location.hash) { current_screen = window.location.hash ; }
-                else { current_screen = '#post-' + contents [0] ['slug']; }
-                $(document) .ready ( function () {
-                    setTimeout (transition_in (current_screen, current_screen), 1000); //???
-                    scroll_listener();
+                    break;
                 }
-                );
-
-                function scroll_listener () {
-                    var delta = 12;
-                    $(window) .bind ('DOMMouseScroll mousewheel', function (event) {
-                        if (Math.abs(event.originalEvent.wheelDelta) < delta || Math.abs (event.originalEvent.detail) < delta) {
-                            return;
-                        }
-                            return;
-
-                        if (event.originalEvent.wheelDelta > 0) {
-                            for (x in contents) {
-                                if ('#post'+contents [x] ['slug'] == current_screen) {
-                                    if (x+1 in contents) {
-                                        transition ('#post-' + contents [x+1] ['slug']);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                )
-                }
+            }
+            scroll_value = 0;
+        }
+    }
+)
+}
 
 
 
-                $ ('.blog-menu li') .hover ( function () {
-                    $ (this) .css ('border', '2px solid rgba(128, 128, 128, 1.0')
-                    $ (this) .css ('z-index', '1000')
-                }
-                , function () {
-                    $ (this) .css ('border', '2px solid rgba(128, 128, 128, 0.0')
-                    $ (this) .css ('z-index', '0')
-                }
-                );
+$ ('.blog-menu li') .hover ( function () {
+    $ (this) .css ('border', '2px solid rgba(128, 128, 128, 1.0')
+    $ (this) .css ('z-index', '1000')
+}
+, function () {
+    $ (this) .css ('border', '2px solid rgba(128, 128, 128, 0.0')
+    $ (this) .css ('z-index', '0')
+}
+);
+
+$(window).resize(function() {
+    return $('.fullscreen').each(function() {
+        return $(this).css({
+            'height': $(window).height() - (
+                parseInt($(this).css('margin-bottom')
+                    ) ) - $(this).offset().top 
+        });
+    });
+});
+
+$(document).ready(function() {
+    return $(window).resize();
+});
+
 
 </script>
-
-<?php get_footer(); ?>
--- wp-content/themes/rubberfish/post.php
+++ wp-content/themes/rubberfish/post.php
@@ -12,32 +12,30 @@ if (has_post_thumbnail()):
     $thumbnail = True;
 endif;
 ?>
-<article class="blog-post" id="post-<?php echo $post->post_name; ?>" >
-<div class="underlay">
-    <a
-        <?php if ($link_to_post): ?>
-        href="/page-<?php echo $post->post_name; ?>"
-        <?php elseif (get_category_by_slug($post->post_name)): ?>
-        href="/category/<?php echo $post->post_name ; ?>"
-        <?php endif ; ?> >
-        <div class="lining">
+<article class="blog-post slide paper fullscreen margins" id="post-<?php echo $post->post_name; ?>" >
+        <a class="fill underlay background-image"
+            <?php if ($link_to_post): ?>
+            href="/page-<?php echo $post->post_name; ?>"
+            <?php elseif (get_category_by_slug($post->post_name)): ?>
+            href="/category/<?php echo $post->post_name ; ?>"
+            <?php endif ; ?>
             <?php if ($thumbnail): ?>
-                <?php $thumbnail_url = wp_get_attachment_image_src((get_post_thumbnail_id($post->ID)), 'post-thumbnail', false)[0]; ?>
-                <div class="background-image" style='background: url("<?php echo $thumbnail_url; ?>")'></div>
+            <?php $thumbnail_url = wp_get_attachment_image_src(
+                        (get_post_thumbnail_id($post->ID)), 'post-thumbnail', false)[0]; ?>
+            style='background-image: url("<?php echo $thumbnail_url; ?>")'
             <?php endif; ?>
+        >
+        </a>
+
+    <div class="content">
+
+        <h3>
+            <span href="<?php the_permalink(); ?>"><?php the_title(); ?></span>
+        </h3>
+        <div >
+            <?php the_content(); ?>
         </div>
-    </a>
-</div>
-
-<div class="content">
-
-    <h3>
-        <span href="<?php the_permalink(); ?>"><?php the_title(); ?></span>
-    </h3>		
-    <div >
-        <?php the_content(); ?>
     </div>
-</div>
 
 </article>
 
