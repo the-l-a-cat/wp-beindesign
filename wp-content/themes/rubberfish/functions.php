54ee9bf4335526b721fe6728c9a4c55a537e2f2c
root <root@web-temp.ngw>
2015-03-16 09:21:10 +0000

    A few modifications in index post display.

--- wp-content/themes/rubberfish/functions.php
+++ wp-content/themes/rubberfish/functions.php
@@ -5,15 +5,13 @@ add_filter( 'display_posts_shortcode_output', 'be_display_posts_portfolio', 10, 
 function be_display_posts_portfolio( $output, $atts, $image, $title, $date, $excerpt, $inner_wrapper ) {
 
     $output = '<' . $inner_wrapper . ' class="listing-item">
-        <div class="index-post">
-        <div class="underlay">
-            <div class="lining">
-                ' . $image .'
+        <div class="index-card">
+            <div class="index-card-image">
+                    ' . $image .'
             </div>
-        </div>
-        <div class="content">
-            ' . $title . $date . $excerpt . '
-        </div>
+            <div class="index-card-content">
+                ' . $title . $date . $excerpt . '
+            </div>
         </div>
     </' . $inner_wrapper . '>';
     // Finally we'll return the modified output
